b77ec46f7e27c1a5afa7403e2cfbca4346c4fb3e
lizeqx <125354381+lizeqx@users.noreply.github.com>
2024-04-13 22:58:20 +0200

    form

--- myproject/myapp/forms.py
+++ myproject/myapp/forms.py
@@ -1,10 +1,9 @@
 from django import forms
 
+from .models import DeviceIssue
 
-class DeviceIssueForm(forms.Form):
-    name = forms.CharField(max_length=100, label='Name')
-    surname = forms.CharField(max_length=100, label='Surname')
-    email = forms.EmailField(label='Email')
-    device_issue = forms.CharField(widget=forms.Textarea, label='Device Issue')
-    serial_number = forms.CharField(max_length=100, label='Serial Number')
-    category = forms.ChoiceField(choices=[('laptop', 'Laptop'), ('phone', 'Phone')], label='Category')
+class DeviceIssueForm(forms.ModelForm):
+    class Meta:
+        model = DeviceIssue
+        fields = ['name', 'surname', 'email', 'country', 'device_issue', 'serial_number', 'category']
+
--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class DeviceIssue(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    email = models.EmailField()
+    country = models.CharField(max_length=100)
+    device_issue = models.TextField()
+    serial_number = models.CharField(max_length=100)
+    category = models.CharField(max_length=50)
+    device_image = models.ImageField(upload_to='device_images/', blank=True, null=True)
+    
+    def __str__(self):
+        return f'{self.name} {self.surname} - {self.email}'
+    
